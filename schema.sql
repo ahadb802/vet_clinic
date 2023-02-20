8d137d76e04ee4dfa61bb148c09c7cdffc0b95bc
Abdul Ahad bhatti <110728909+ahadb802@users.noreply.github.com>
2023-02-20 22:00:18 +0500

    make table for animal

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+CREATE SCHEMA `vet_clinic` ;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE `vet_clinic`.`animals` (
+  `idanimals` INT NULL,
+  `name` VARCHAR(100) NULL,
+  `date_of_birth` DATE NULL,
+  `escape_attempts` INT NULL,
+  `neutered` BOOLEAN NULL,
+  `weight_kg` DECIMAL() NULL);
