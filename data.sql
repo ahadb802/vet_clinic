75da918e249c8716389edbbb80a812bd11d31286
Abdul Ahad bhatti <110728909+ahadb802@users.noreply.github.com>
2023-02-22 21:41:16 +0500

    populate animal table

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO vet_clinic.animals(A_id,A_name,A_DOB,A_escape_attempts,neutered,weight_kg) VALUES (0,'Agumon', '2022-02-03', 0, TRUE, 10.23);
+INSERT INTO vet_clinic.animals(A_id,A_name,A_DOB,A_escape_attempts,neutered,weight_kg) VALUES (1,'Gabumon', '20218-11-15', 2, TRUE, 8.0);
+INSERT INTO vet_clinic.animals(A_id,A_name,A_DOB,A_escape_attempts,neutered,weight_kg) VALUES (2,'Pikachu', '2021-01-07', 1, FALSE, 15.04);
+INSERT INTO vet_clinic.animals(A_id,A_name,A_DOB,A_escape_attempts,neutered,weight_kg) VALUES (3,'Devimon', '2017-05-12', 5, TRUE, 11.0);
