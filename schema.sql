4e9137ff02836245569ee6529330eed2f629bf58
Abdul Ahad bhatti <110728909+ahadb802@users.noreply.github.com>
2023-02-22 21:22:52 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 CREATE SCHEMA `vet_clinic` ;
 
-CREATE TABLE `vet_clinic`.`animals` (
-  `idanimals` INT NULL,
-  `name` VARCHAR(100) NULL,
-  `date_of_birth` DATE NULL,
-  `escape_attempts` INT NULL,
-  `neutered` BOOLEAN NULL,
-  `weight_kg` DECIMAL() NULL);
+CREATE TABLE vet_clinic.animals (
+A_id INT NULL,
+A_name varchar(100) NULL,
+A_DOB varchar(100) NULL,
+A_escape_attempts INT NULL,
+neutered BOOLEAN DEFAULT NULL,
+weight_kg DECIMAL(4,2) NULL
+);
+
